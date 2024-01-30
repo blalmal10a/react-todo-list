d90d2a54f115f05261c11f813b1a45b6a8d6eb83
ys-grouse <malsawma7777@gmail.com>
2024-01-31 00:51:31 +0530

    init react, vite, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import {useState} from "react"
+
+import "./App.css"
+
+function App() {
+	// const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<div className="dark:bg-blue-50">
+				<h1 className="text-3xl">TODO LIST</h1>
+			</div>
+		</>
+	)
+}
+
+export default App
